db263b7b194c811ac1241516f8a2b66855309f89
Victor Gan <victorgan@gmail.com>
2015-05-13 15:51:10 -0700

    comments

--- makeWheelchairShape.m
+++ makeWheelchairShape.m
@@ -1,8 +1,11 @@
 function [wheelchairShapeAngle] = makeWheelchairShape(wheelchairsize, angles)
     numAngles = size(angles, 2);
 
-    padsize = [10 10]; % surrounding 0's. Larger allows for larger rotations
+    % Pad rectangle with zero's. Larger allows for larger rotations
+    % Needed to ensure all rotations have equal size, instead of relying on imrotate
+    padsize = [10 10]; 
     wheelchairShape = padarray(ones(wheelchairsize(1),wheelchairsize(2)), padsize); % at 0 degrees 
+
     wheelchairShapeAngle = zeros(size(wheelchairShape,1),size(wheelchairShape,2),numAngles);
     for i = 1:numAngles
         wheelchairShapeAngle(:,:,i) = imrotate(wheelchairShape, angles(i), 'bicubic', 'crop');
