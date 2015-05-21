e8e359b6b28f91d15a4ef0fb4f09fb17afb1e295
Victor Gan <victorgan@gmail.com>
2015-05-21 14:00:43 -0700

    minor refactoring

--- rosbagToImages.m
+++ rosbagToImages.m
@@ -3,16 +3,14 @@
 % load Bag
 % ----------------------------------------
 bagFolder = '/media/vgan/stashpile/datasets/gan2015wheelchair/2015-03-22-wheelchairtests'
-% bagfile = '2015-03-22-16-41-16.bag';
-bagfile = '2015-03-22-16-37-50';
+bagfile = '2015-03-22-16-41-16';
+% bagfile = '2015-03-22-16-37-50';
 bagFullFile = fullfile(bagFolder, [bagfile '.bag']);
 
 projectFolder = '/home/vgan/code/datasets/gan2015wheelchair';
-outputFolder = fullfile(projectFolder, [bagfile '-temp']);
+outputFolder = fullfile(projectFolder, [bagfile]);
 
-[points, imgRGB] = rosbagToPointCloud(bagFullFile, outputFolder);
-
-outputFolder = fullfile(projectFolder, [bagfile '-temp2']);
+% [points, imgRGB] = rosbagToPointCloud(bagFullFile, outputFolder);
 [points, imgRGB] = rosbagToPointCloud2(bagFullFile, outputFolder);
 
 % p = pointCloud(points, 'Color', imgRGB);
