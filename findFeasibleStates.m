4543f40ec465ab873b28d79d2d37d5445b126778
Victor Gan <victorgan@gmail.com>
2015-08-06 12:03:57 -0700

    better boundary check

--- findFeasibleStates.m
+++ findFeasibleStates.m
@@ -5,29 +5,7 @@ function [feasibleStates2] = findFeasibleStates(groundMap, wheelchairShapeAngle,
     [numRows, numCols] = size(groundMap);
     numAngles = size(wheelchairShapeAngle,3);
 
-    wheelChairMaps = getWheelChairMaps(wheelchairShapeAngle, numRows, numCols);
-
     % % TODO RRT if needed
-
-    % feasibleStates = false(numRows, numCols, numAngles);
-    % for thetaIdx = 1:numAngles
-    % for r = 1:numRows
-    % for c = 1:numCols
-    %     feasible = true;
-    %     wheelChairMap = wheelChairMaps{r, c, thetaIdx};
-
-    %     % If any wheelChairMap isn't fully on the ground, it's infeasible.
-    %     negGroundMap = ~groundMap; % free space == 0
-    %     collisions = negGroundMap(wheelChairMap) == 1;
-    %     if any(collisions)
-    %         feasible = false;
-    %     end 
-
-    %     feasibleStates(r, c, thetaIdx) = feasible;
-    % end % for
-    % end % for
-    % end % for
-
 
     negGroundMap = ~groundMap; % free space == 0
     feasibleStates = false(numRows, numCols, numAngles);
@@ -48,43 +26,12 @@ function [feasibleStates2] = findFeasibleStates(groundMap, wheelchairShapeAngle,
     feasibleStates2 = feasibleStates & filledRegion; % region connected to origin
 
     % Not feasible if wheelchair is not fully on map
-    feasibleStates3 = false(numRows, numCols, numAngles);
+    % If wheelchair collides with borders, state is not feasible
+    nonBorderStates = false(numRows, numCols);
     for thetaIdx = 1:numAngles
-        centreWheelChairMap = wheelChairMaps{floor(numRows/2), floor(numCols/2), thetaIdx};
-        wheelChairArea = sum(centreWheelChairMap(:));
-        for r = 1:numRows
-        for c = 1:numCols
-            wheelChairMap = wheelChairMaps{r, c, thetaIdx};
-
-            % If convolution is cut off, it's infeasible
-            if sum(wheelChairMap(:)) < wheelChairArea
-                feasibleStates3(r, c, thetaIdx) = false;
-            elseif sum(wheelChairMap(:)) > wheelChairArea
-                error('bad wheelchair size');
-            else
-                feasibleStates3(r, c, thetaIdx) = true;
-            end % if
-        end % for
-        end % for
+        boundaryValue = 1;
+        boundaryStates(:,:,thetaIdx) = imfilter(nonBorderStates, wheelchairShapeAngle(:,:,thetaIdx), boundaryValue);
     end % for
 
-    feasibleStates2(~feasibleStates3) = false;
-end % function
-
-function isFeasibleState = overlapsWheelchair(r, c, thetaIdx, wheelchairShapeAngle, numRows, numCols, groundMap)
-    % impulseMap = zeros(numRows,numCols);
-    % impulseMap(r,c) = 1;
-
-    % placedWheelchair = conv2(impulseMap, wheelchairShapeAngle(:,:,thetaIdx),'same');
-
-    % negGroundMap = ~groundMap; % free space == 1
-    % collisions = negGroundMap(placedWheelchair ~= 0) == 1;
-
-    % if (groundMap(r,c))
-    % % if any(collisions)
-    %     isFeasibleState = false;
-    % else
-    %     isFeasibleState = true;
-    % end 
-    isFeasibleState = groundMap(r,c);
+    feasibleStates2(boundaryStates) = false;
 end % function
