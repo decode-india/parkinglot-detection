450cd003b69a27050ea18ada22e514d99a0cf46d
Victor Gan <victorgan@gmail.com>
2015-08-06 12:23:08 -0700

    added padding option

--- makeWheelchairShape.m
+++ makeWheelchairShape.m
@@ -1,9 +1,15 @@
-function [wheelchairShapeAngle] = makeWheelchairShape(wheelchairsize, angles)
+function [wheelchairShapeAngle] = makeWheelchairShape(wheelchairsize, angles, padsize)
+    if nargin == 2
+        defaultPadsize = [10 10];
+        padsize = defaultPadsize; 
+    elseif nargin ~= 3
+        error('incorrect number of input arguments');
+    end
+
     numAngles = size(angles, 2);
 
     % Pad rectangle with zero's. Larger allows for larger rotations
     % Needed to ensure all rotations have equal size, instead of relying on imrotate
-    padsize = [10 10]; 
     wheelchairShape = padarray(ones(wheelchairsize(1),wheelchairsize(2)), padsize); % at 0 degrees 
 
     wheelchairShapeAngle = zeros(size(wheelchairShape,1),size(wheelchairShape,2),numAngles);
